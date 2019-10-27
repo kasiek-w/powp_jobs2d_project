29324b26a6542d531090927f9fdd7fc11a67bdf8
Katarzyna Wójcik <180311@edu.p.lodz.pl>
2019-10-27 17:02:33 +0100

    3.2 - adding setPosition into MyAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -26,7 +26,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
+		setPosition(x, y);
 		drawLine(line);
 	}
 
